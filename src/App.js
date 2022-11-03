a233332945b21c19eae40e4dceefc20a7bb656bd
Robiyaxon <tuhtaevarobiahon@gmail.com>
2022-11-03 18:19:07 +0500

    pull

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-
+import Register from "./components/about/register/Register";
+import React from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Register/>
     </div>
   );
 }

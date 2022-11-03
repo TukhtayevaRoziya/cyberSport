42331cf6c587fe960e91e9c921d423a0788e90bc
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-11-03 18:21:55 +0500

    fonts

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 
-import PhotoIcon from '@material-ui/icons/Photo';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import GroupWorkIcon from '@material-ui/icons/GroupWork';
-import LocationCityIcon from '@material-ui/icons/LocationCity';
+import PhotoIcon from '@material-ui/icons/Photo'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import GroupWorkIcon from '@material-ui/icons/GroupWork'
+import LocationCityIcon from '@material-ui/icons/LocationCity'
 
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
 import BottomNavigation from '@material-ui/core/BottomNavigation'
@@ -16,7 +16,7 @@ import styles from './Navbar.module.css'
 export default class Navbar extends Component {
   state = {
     value: 0,
-    pathMap: ['#about', '/tournament', '/abacus', '/photo'],
+    pathMap: ['/#about', '/#tournament', '/#abacus', '/#photo'],
     width: 0,
     height: 0,
   }
@@ -24,22 +24,25 @@ export default class Navbar extends Component {
   handleChange = (event, value) => {
     this.setState({ value })
   }
+
   updateDimensions = () => {
     this.setState({ width: window.innerWidth, height: window.innerHeight })
   }
+
   componentDidMount() {
     window.addEventListener('resize', this.updateDimensions)
   }
   componentWillUnmount() {
     window.removeEventListener('resize', this.updateDimensions)
   }
+
   render() {
     const { value, pathMap } = this.state
     const data = [
       { id: 0, value: 'О нас', icon: <LocationCityIcon /> },
-      { id: 1, value: 'Турниры', icon: <GroupWorkIcon />},
-      { id: 2, value: 'Счёты', icon: <EventNoteIcon />},
-      { id: 3, value: 'Фото', icon: <PhotoIcon />},
+      { id: 1, value: 'Турниры', icon: <GroupWorkIcon /> },
+      { id: 2, value: 'Счёты', icon: <EventNoteIcon /> },
+      { id: 3, value: 'Фото', icon: <PhotoIcon /> }
     ]
 
     const dataMap = data.map((d) => (
@@ -51,9 +54,13 @@ export default class Navbar extends Component {
         to={pathMap[d.id]}
       />
     ))
+
     return (
       <div className={styles.wrap}>
-      <img src={icon} alt=''/>
+        <div className={styles.img_wrap}>
+          <img src={icon} alt="" />
+        </div>
+
         <BottomNavigation
           value={value}
           onChange={this.handleChange}
@@ -61,6 +68,7 @@ export default class Navbar extends Component {
         >
           {dataMap}
         </BottomNavigation>
+        <button className={styles.btn}>Регистрация</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom/dist'
+import { BrowserRouter } from 'react-router-dom/dist'
 import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )

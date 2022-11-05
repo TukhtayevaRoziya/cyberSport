63231ac0d37cba2379bce1c031088502acc02681
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-11-05 09:05:44 +0500

    photo

---
+++ src/components/registration/Registration.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Registration.module.css'
+
+const Registration = () => {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.block}>
+
+      </div>
+    </div>
+  )
+}
+
+export default Registration

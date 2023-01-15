426c0b4a4d6eee20c396431136bb365fad5cb05b
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2023-01-15 09:47:53 +0500

    change yarn file

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index_bundle.js',
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.[contenthash].js",
+    clean: true,
   },
-  stats: 'errors-only',
+  devtool: "source-map",
   // resolve: {
   //   extensions: ['.js', '.jsx', '.ts', '.tsx', '.png', '.jpg', 'webp'],
   // },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.ts', '.tsx']
-},
+    extensions: ['', '.js', '.jsx', '.ts', '.tsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -26,19 +33,22 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
-
       {
-        test: /\.(jpe?g|png|gif|woff|woff2|otf|eot|ttf|svg|webp)(\?[a-z0-9=.]+)?$/,
-        use: [
-            {
-                loader: 'url-loader',
-                options: {
-                    limit: 1000,
-                    name : 'assets/img/[name].[ext]'
-                }
-            }
-        ]
-    }
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+      //   {
+      //     test: /\.(jpe?g|png|gif|woff|woff2|otf|eot|ttf|svg|webp)(\?[a-z0-9=.]+)?$/,
+      //     use: [
+      //         {
+      //             loader: 'url-loader',
+      //             options: {
+      //                 limit: 1000,
+      //                 name : 'assets/img/[name].[ext]'
+      //             }
+      //         }
+      //     ]
+      // }
     ],
   },
 }
